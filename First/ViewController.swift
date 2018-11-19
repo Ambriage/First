e80867e90db21d1d6954ef0ecb468fe1887c4cac
Bart Kerckx <bart.kerckx@icloud.com>
2018-11-19 20:46:13 +0100

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Bart Kerckx on 19/11/2018.
+//  Copyright © 2018 Bake Street. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
